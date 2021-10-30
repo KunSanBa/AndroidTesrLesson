86f9d70b757dbfcff14d711896daa8c21c5a3d0d
Aleksandr <Kunsanba@gmail.com>
2021-10-30 08:17:31 +0300

    Add TextView, Button, FrameLayout on activity_main.xml

--- app/src/main/java/com/aleksandrkunevich/android/androidtestlesson/MainActivity.kt
+++ app/src/main/java/com/aleksandrkunevich/android/androidtestlesson/MainActivity.kt
@@ -1,8 +1,16 @@
+/*
+2. Реализовать activity с заголовком (TextView) и одной кнопкой.
+Заголовок вверху экрана, кнопка внизу экрана.
+По первому нажатию на кнопку открывается первый фрагмент, по второму второй, по третьему третий.
+Контент внутри фрагментов произвольный. Фоны фрагментов разных цветов.
+
+Добавить отступы: кнопки от низа, TextView от верха. Значения отступы произвольны
+ */
+
 package com.aleksandrkunevich.android.androidtestlesson
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
