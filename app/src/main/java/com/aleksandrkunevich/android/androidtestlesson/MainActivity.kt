868924621d36c762330d5efb5bb1e24dc7777738
Aleksandr <Kunsanba@gmail.com>
2021-10-30 07:33:20 +0300

    Initial commit

---
+++ app/src/main/java/com/aleksandrkunevich/android/androidtestlesson/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.aleksandrkunevich.android.androidtestlesson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        btn.setOnClickListener {
+            openFragment()
+        }
+
+    }
+
+    private fun openFragment() {
+        supportFragmentManager
+            .beginTransaction()
+            .addToBackStack(FirstFragment.TAG)
+            .add(R.id.container, FirstFragment.newInstance())
+            .commit()
+    }
+
+}
